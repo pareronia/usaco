06a21567ccff311720fa16acc8b7f6f06a1ddcaf
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-28 00:33:17 +0100

    Problem 687

---
+++ src/main/java/com/github/pareronia/usaco/_687/DontBeLast.java
@@ -0,0 +1,193 @@
+package com.github.pareronia.usaco._687;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.function.Supplier;
+
+/**
+ * Problem 1. Don't Be Last!
+ * @see <a href="http://www.usaco.org/index.php?page=viewproblem2&cpid=687">http://www.usaco.org/index.php?page=viewproblem2&cpid=687</a>
+ */
+public class DontBeLast {
+
+    private static final Set<String> COWS = Set.of("Bessie", "Elsie", "Daisy",
+            "Gertie", "Annabelle", "Maggie", "Henrietta");
+    
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public DontBeLast(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final Map<String, Integer> map = new HashMap<>();
+        for (int j = 0; j < n; j++) {
+            map.merge(sc.next(), sc.nextInt(), Integer::sum);
+        }
+        for (final String cow : COWS) {
+            map.merge(cow, 0, Integer::sum);
+        }
+        log(() -> map);
+        final List<Integer> m
+                = map.values().stream().distinct().sorted().collect(toList());
+        final String ans;
+        if (m.size() == 1) {
+            ans = "Tie";
+        } else {
+            final List<String> notLast = map.entrySet().stream()
+                .filter(e -> e.getValue() == m.get(1))
+                .map(Entry::getKey)
+                .collect(toList());
+            ans = notLast.size() > 1 ? "Tie" : notLast.get(0);
+        }
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (isSample()) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = DontBeLast.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = DontBeLast.class.getResourceAsStream("notlast.in");
+            out = new PrintStream(new FileOutputStream("notlast.out"), true);
+        }
+        
+        new DontBeLast(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(DontBeLast.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("usaco"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @SuppressWarnings("unused")
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+        
+        @SuppressWarnings("unused")
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/pareronia/usaco/_687/DontBeLastTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._687;
+
+import com.github.pareronia.usaco.TestBase;
+
+class DontBeLastTest extends TestBase<DontBeLast> {
+
+    protected DontBeLastTest() {
+        super(DontBeLast.class);
+    }
+}
