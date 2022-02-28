991162e6cb4d9e9e0a32b029cc0d4c99d8e19211
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-28 01:00:02 +0100

    Problem 667

---
+++ src/main/java/com/github/pareronia/usaco/_667/CitiesAndStates.java
@@ -0,0 +1,157 @@
+package com.github.pareronia.usaco._667;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Problem 2. Cities and States
+ * @see <a href="http://www.usaco.org/index.php?page=viewproblem2&cpid=667">http://www.usaco.org/index.php?page=viewproblem2&cpid=667</a>
+ */
+public class CitiesAndStates {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public CitiesAndStates(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final Map<String, Integer> map = new HashMap<>();
+        for (int j = 0; j < n; j++) {
+            final String city = sc.next().substring(0, 2);
+            final String state = sc.next();
+            if (city.equals(state)) {
+                continue;
+            }
+            map.merge(city + state, 1, Integer::sum);
+        }
+        final int ans = map.keySet().stream()
+            .mapToInt(k -> {
+                final String revk = k.substring(2) + k.substring(0, 2);
+                return map.get(k) * map.getOrDefault(revk, 0);
+            })
+            .sum() / 2;
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (isSample()) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = CitiesAndStates.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = CitiesAndStates.class.getResourceAsStream("citystate.in");
+            out = new PrintStream(new FileOutputStream("citystate.out"), true);
+        }
+        
+        new CitiesAndStates(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(CitiesAndStates.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("usaco"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/pareronia/usaco/_667/CitiesAndStatesTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._667;
+
+import com.github.pareronia.usaco.TestBase;
+
+class CitiesAndStatesTest extends TestBase<CitiesAndStates> {
+
+    protected CitiesAndStatesTest() {
+        super(CitiesAndStates.class);
+    }
+}
