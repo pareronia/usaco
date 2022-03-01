071079c514e08678c7b2d0e9c074b15b003e4d0d
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-01 13:41:58 +0100

    Problem 891

---
+++ src/main/java/com/github/pareronia/usaco/_891/ShellGame.java
@@ -0,0 +1,166 @@
+package com.github.pareronia.usaco._891;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Problem 1. Shell Game
+ * @see <a href="http://www.usaco.org/index.php?page=viewproblem2&cpid=891">http://www.usaco.org/index.php?page=viewproblem2&cpid=891</a>
+ */
+public class ShellGame {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public ShellGame(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        int n = sc.nextInt();
+        int[][] a = new int[n][3];
+        for (int j = 0; j < n; j++) {
+            a[j] = sc.nextIntArray(3);
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int k = 1; k <= 3; k++) {
+            int points = 0;
+            int p = k;
+            for (int j = 0; j < n; j++) {
+                if (a[j][0] == p) {
+                    p = a[j][1];
+                } else if (a[j][1] == p) {
+                    p = a[j][0];
+                }
+                points += (p == a[j][2] ? 1 : 0);
+            }
+            ans = Math.max(ans, points);
+        }
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (isSample()) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = ShellGame.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = ShellGame.class.getResourceAsStream("shell.in");
+            out = new PrintStream(new FileOutputStream("shell.out"), true);
+        }
+        
+        new ShellGame(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(ShellGame.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("usaco"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        } 
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/pareronia/usaco/_891/ShellGameTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._891;
+
+import com.github.pareronia.usaco.TestBase;
+
+class ShellGameTest extends TestBase<ShellGame> {
+
+    protected ShellGameTest() {
+        super(ShellGame.class);
+    }
+}
