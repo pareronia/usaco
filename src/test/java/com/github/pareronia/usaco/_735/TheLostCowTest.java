d31f3b2193c29ad1f98cc784a9186afe7ea7e3f0
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-03 18:31:05 +0100

    Problem 735

---
+++ src/test/java/com/github/pareronia/usaco/_735/TheLostCowTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._735;
+
+import com.github.pareronia.usaco.TestBase;
+
+class TheLostCowTest extends TestBase<TheLostCow> {
+
+    protected TheLostCowTest() {
+        super(TheLostCow.class);
+    }
+}
