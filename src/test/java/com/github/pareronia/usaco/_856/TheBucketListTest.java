4321416cec99fd373d45fd307c1b98dcc04da4b9
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-05 11:20:51 +0100

    Problem 856

---
+++ src/test/java/com/github/pareronia/usaco/_856/TheBucketListTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._856;
+
+import com.github.pareronia.usaco.TestBase;
+
+class TheBucketListTest extends TestBase<TheBucketList> {
+
+    protected TheBucketListTest() {
+        super(TheBucketList.class);
+    }
+}
