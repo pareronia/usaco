efddeb5b8a3734ff9b85da1801cd5f1eda64c28c
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-06 00:40:51 +0100

    Problem 639

---
+++ src/test/java/com/github/pareronia/usaco/_639/DiamondCollectorTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._639;
+
+import com.github.pareronia.usaco.TestBase;
+
+class DiamondCollectorTest extends TestBase<DiamondCollector> {
+
+    protected DiamondCollectorTest() {
+        super(DiamondCollector.class);
+    }
+}
