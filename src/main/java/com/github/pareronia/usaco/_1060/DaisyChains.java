e9359081867dab4ca8486026912f10d22f553b29
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-07 15:41:19 +0100

    Problem 1060

---
+++ src/main/java/com/github/pareronia/usaco/_1060/DaisyChains.java
@@ -0,0 +1,161 @@
+package com.github.pareronia.usaco._1060;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Problem 2. Daisy Chains
+ * @see <a href="http://www.usaco.org/index.php?page=viewproblem2&cpid=1060">http://www.usaco.org/index.php?page=viewproblem2&cpid=1060</a>
+ */
+public class DaisyChains {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public DaisyChains(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+    	int n = sc.nextInt();
+    	int[] a = sc.nextIntArray(n);
+    	int ans = 0;
+    	for (int j = 0; j < n; j++) {
+    		for (int k = j; k < n; k++) {
+    			final int[] b = Arrays.copyOfRange(a, j, k + 1);
+    			Arrays.sort(b);
+    			final double avg = Arrays.stream(b).average().orElseThrow();
+    			if ((avg == Math.floor(avg)) && !Double.isInfinite(avg)) {
+    				if (Arrays.binarySearch(b, Double.valueOf(avg).intValue()) >= 0) {
+    					ans++;
+    				}
+    			}
+    		}
+    	}
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (isSample()) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = DaisyChains.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new DaisyChains(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(DaisyChains.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("usaco"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/pareronia/usaco/_1060/DaisyChainsTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._1060;
+
+import com.github.pareronia.usaco.TestBase;
+
+class DaisyChainsTest extends TestBase<DaisyChains> {
+
+	protected DaisyChainsTest() {
+		super(DaisyChains.class);
+	}
+}
