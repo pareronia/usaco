adea197fcb3232cac12725444cf6152ef0579624
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-08 11:57:29 +0100

    Problem 963

---
+++ src/test/java/com/github/pareronia/usaco/_963/CowGymnasticsTest.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.usaco._963;
+
+import com.github.pareronia.usaco.TestBase;
+
+class CowGymnasticsTest extends TestBase<CowGymnastics> {
+
+	protected CowGymnasticsTest() {
+		super(CowGymnastics.class);
+	}
+}
