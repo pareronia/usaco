02955547c7a5f0333325939317f5866e30bc60cc
pareronia <49491686+pareronia@users.noreply.github.com>
2022-04-12 19:20:35 +0200

    Problem 572

---
+++ src/main/java/com/github/pareronia/usaco/_572/BreedCounting.java
@@ -0,0 +1,153 @@
+package com.github.pareronia.usaco._572;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Problem 3. Breed Counting
+ * @see <a href="http://www.usaco.org/index.php?page=viewproblem2&cpid=572">http://www.usaco.org/index.php?page=viewproblem2&cpid=572</a>
+ */
+public class BreedCounting {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public BreedCounting(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final Integer i, final FastScanner sc) {
+        final int n = sc.nextInt();
+        final int q = sc.nextInt();
+        final int[][] br = new int[3][n + 1];
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < 3; k++) {
+                br[k][j + 1] = br[k][j];
+            }
+            br[sc.nextInt() - 1][j + 1]++;
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < q; j++) {
+            final int a = sc.nextInt();
+            final int b = sc.nextInt();
+            sb.append(br[0][b] - br[0][a - 1]).append(" ")
+                .append(br[1][b] - br[1][a - 1]).append(" ")
+                .append(br[2][b] - br[2][a - 1])
+                .append(System.lineSeparator());
+        }
+        final String ans = sb.toString();
+        this.out.print(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = isSample() ? sc.nextInt() : 1;
+            for (int i = 0; i < numberOfTestCases; i++) {
+                handleTestCase(i, sc);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = BreedCounting.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = BreedCounting.class.getResourceAsStream("bcount.in");
+            out = new PrintStream(new FileOutputStream("bcount.out"), true);
+        }
+        
+        new BreedCounting(sample, is, out).solve();
+        
+        out.flush();
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(BreedCounting.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+    
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("usaco"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+    
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
